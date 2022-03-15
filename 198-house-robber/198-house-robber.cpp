8d5ac4181d3860518a20bd3616ee2c0be7ff6c95
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-03-15 22:15:47 +0530

    Time: 4 ms (35.09%), Space: 9.2 MB (5.10%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int dp[1002];
-    int dps(vector<int>v ,  int n )
+    int dp[1001];
+    int maxi(vector<int>nums,int n)
     {
-         if(n<0)
-         {
-             return 0;
-         }
-        if(dp[n]==-1)
+        if(n<0)
         {
-            dp[n]= max(v[n] + dps(v,n-2) , dps(v,n-1));
+            return 0;
         }
-        return dp[n];
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        else
+        {
+            return dp[n]=max( maxi(nums,n-1) , nums[n]+maxi(nums,n-2) );
+        }
     }
-    
     
     int rob(vector<int>& nums) {
         memset(dp,-1,sizeof(dp));
-        
-        return dps(nums,nums.size()-1);
-        
+        return maxi(nums,nums.size()-1);
     }
 };
-//
