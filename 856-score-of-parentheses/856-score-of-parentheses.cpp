dac963b2a79697f407498589d9a9fe050a4baf97
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-03-17 10:40:27 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (45.05%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        int res=0;
+        stack<int>se;
+        for(auto x:s)
+        {
+            if(x=='(')
+            {
+                se.push(res);
+                res=0;
+            }
+            else
+            {
+                res=se.top() + max(res*2,1);
+                se.pop();
+            }
+        }
+        return res;        
+    }
+};
