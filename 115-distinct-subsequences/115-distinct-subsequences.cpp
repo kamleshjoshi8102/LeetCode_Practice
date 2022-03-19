01be453b3e69cda5eb09b2f0d66f012d0ad3b89d
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-03-19 11:25:38 +0530

    Time: 47 ms (54.44%), Space: 30.1 MB (14.00%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,24 +1,63 @@
 class Solution {
 public:
-    int f(string&s,string &t,int i,int j,vector<vector<int>>&v){
-        if(j<0) return 1; 
-        if(i<0) return 0;
-        if(v[i][j]!=-1){
-            return v[i][j];
-        }
-        int pick=0; 
-        int notpick=0; 
-        if(s[i]==t[j]){
-            pick=f(s,t,i-1,j-1,v); 
-            notpick=f(s,t,i-1,j,v);
-        }
-        else{
-            notpick=f(s,t,i-1,j,v);
-        }
-        return v[i][j]=pick+notpick; 
-    }
+    
+    /*
+        if those elements are equal then I have only 2 choices
+            include it or not 
+            so we will pass two recursion function in which we will include in first and not include in second
+            if not equal then we will not include and return it's length -1 
+    
+    */
+    int cn=0;
+
+//     int sub(string s, string t,int n , int m , int dp[][1002])
+//     { 
+//         if(m==0)
+//         {
+//             return 1;
+//         }
+//         if(n==0)
+//         {
+//             return 0;
+//         }
+//         if(dp[n][m]!=-1)
+//         {
+//             return dp[n][m];
+//         }
+        
+//         if(s[n-1]==t[m-1])
+//         {
+//             return dp[n][m] = sub(s,t,n-1,m-1,dp) + sub(s,t,n-1,m,dp);
+//         }
+//         else
+//         {
+//             return dp[n][m]=sub(s,t,n-1,m,dp);
+//         }
+        
+//     }
+    
     int numDistinct(string s, string t) {
-        vector<vector<int>>v(s.size(),vector<int>(t.size(),-1));
-        return f(s,t,s.size()-1,t.size()-1,v);
+        int n=s.size();
+        int m=t.size();
+        
+ vector<vector<double>>dp(n+1,vector<double>(m+1,0));
+         
+   for(int i=0;i<=n;i++)
+        dp[i][0]=1;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
