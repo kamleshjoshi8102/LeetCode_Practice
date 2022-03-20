afa94e23bebcf5a5935d89bbaab261d376be75c8
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-03-20 23:05:03 +0530

    Time: 6 ms (77.34%), Space: 8.8 MB (69.28%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        
+        stack<int>st;
+        unordered_map<int,int>mp;
+        vector<int>v;
+        
+        
+        
+        for(int i=0;i<nums2.size();i++)
+        {
+            while(st.size()>0 and nums2[i]>st.top())
+            {
+                mp[st.top()]=nums2[i];  // means this is greater than you mfs
+                st.pop();
+            }
+            st.push(nums2[i]);
+        }
+        while(!st.empty())
+        {
+            mp[st.top()]=-1;    // means no one is greater than you mfs
+            st.pop();
+        }
+        
+        for(int i=0;i<nums1.size();i++)
+        {
+            v.push_back(mp[nums1[i]]);
+        }
+        return v;
+        
+        
+    }
+};
