42a93764308d233fe2c3283b674344fa16caea2a
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-03-22 12:27:38 +0530

    Time: 34 ms (46.47%), Space: 11.9 MB (60.74%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-    int dp[302][5002];
-    int ch(vector<int>coins  , int n  , int am)
-    {
-        if(n==0)
-        {
-            if(am==0)
-            {
-                return 1;
-            }
-            return 0;
-        }
-        if(dp[n][am]!=-1)
-        {
-            return dp[n][am];
-        }
-        int nt_take=ch(coins,n-1,am);
-        int take=0;
-        if(coins[n-1]<=am)
-        {
-            take=ch(coins,n,am-coins[n-1]);
-        }
-        return dp[n][am]=take+nt_take;
-    }
-    
     int change(int amount, vector<int>& coins) {
-        memset(dp,-1,sizeof(dp));
-        int ans=ch(coins,coins.size(),amount);
-        return ans;
+        // memset(dp,-1,sizeof(dp));
+        int n=coins.size();
+        int dp[n+1][amount+1];
+        
+        
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=1;
+        }
+        for(int j=0;j<=amount;j++)
+        {
+            dp[0][j]=0;
+        }
+            
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                if(coins[i-1]<=j)
+                {
+                    dp[i][j]= dp[i-1][j] + dp[i][j-coins[i-1]]  ;
+                }
+                else
+                {
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        
+     
+        
+        return dp[n][amount];
+ 
     }
 };
