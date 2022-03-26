c0e6bb4476f6823203ee67404f68b42911fe7d92
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-03-26 08:56:41 +0530

    Time: 33 ms (5.16%), Space: 22.1 MB (8.87%) - LeetHub

---
+++ 513-find-bottom-left-tree-value/513-find-bottom-left-tree-value.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        // Idea is to go level wise and then return last level's first value 
+        //  :)
+        int res=0,max_level=0;
+        if(root==NULL)
+        {
+            return 0;
+        }
+        map<int,int>mp;
+        queue<pair<TreeNode*,int>>q;
+        
+        q.push({root,0});
+        
+        while(!q.empty())
+        {
+            auto p=q.front();
+            q.pop();
+            
+            TreeNode*node=p.first;
+            int level=p.second;
+            max_level=max(max_level,level);
+            
+            if(mp.find(level)==mp.end())
+            {
+                mp[level]=node->val;
+            }
+            if(node->left)
+            {
+                q.push({node->left,level+1});
+            }
+            if(node->right)
+            {
+                q.push({node->right,level+1});
+            }
+        }
+        
+        return mp[max_level];
+        
+    }
+};
