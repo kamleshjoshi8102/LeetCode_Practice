c757e16313ed64e36d0eddb869fff8e5f839360a
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-04-04 23:07:20 +0530

    Time: 21 ms (44.93%), Space: 13.2 MB (54.19%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,32 +3,40 @@ public:
     void setZeroes(vector<vector<int>>& mt) {
         int n=mt.size();
         int m=mt[0].size();
-        
-        vector<bool>row(n,false);
-        vector<bool>col(m,false);
+        bool check=false;
         
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
+            if(mt[i][0]==0)
+            {
+                check=true;
+            }
+            for(int j=1;j<m;j++)
             {
                 if(mt[i][j]==0)
                 {
-                    row[i]=true;
-                    col[j]=true;
+                    mt[i][0]=0;
+                    mt[0][j]=0;
                 }
             }
         }
         
-        for(int i=0;i<n;i++)
+        for(int i=n-1;i>=0;i--)
         {
-            for(int j=0;j<m;j++)
+            for(int j=m-1;j>=1;j--)
             {
-                if(row[i]==true || col[j]==true)
+                if(mt[i][0]==0 || mt[0][j]==0)
                 {
                     mt[i][j]=0;
                 }
             }
+            if(check==true)
+            {
+                mt[i][0]=0;
+            }
         }
-        return;        
+      
+        
+        
     }
 };
