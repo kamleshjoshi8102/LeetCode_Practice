bcce929fc943be094c85bb1ba068ae34874bdd77
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-04-05 13:06:48 +0530

    Time: 7 ms (16.03%), Space: 7.2 MB (33.07%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        vector<vector<int>>matrix1=matrix;
+        vector<vector<int>>temp=matrix;
+        int n=matrix.size(),m=matrix[0].size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                temp[i][j]=matrix[j][i];
+            }
+        }
+        matrix=temp;
         
-        int n=matrix[0].size();
-        for(int i=0;i<matrix.size();i++)
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<matrix.size();j++)
-            {
-                matrix1[i][j]=matrix[j][i];
-            }
-            // cout<<endl;
+            reverse(matrix[i].begin(),matrix[i].end());
         }
-        for(int i=0;i<matrix1.size();i++)
-        {
-             for(int j=0;j<matrix1.size()/2;j++)
-                {
-                     swap(matrix1[i][j],matrix1[i][n-j-1]);
-                }
-        }
-        for(int i=0;i<matrix.size();i++)
-        {
-            for(int j=0;j<matrix.size();j++)
-            {
-                cout<<matrix1[i][j]<<' ';
-            }
-            cout<<endl;
-        }
-     matrix = matrix1;
+        
     }
 };
