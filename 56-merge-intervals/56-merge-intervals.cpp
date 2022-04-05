442486abfc2728fb7eb0abaccbe9020ae31f701b
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-04-05 14:21:52 +0530

    Time: 30 ms (96.97%), Space: 19 MB (85.34%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
         vector<vector<int>>res;
-        
         sort(intervals.begin(),intervals.end());
-        
-        int prefir=intervals[0][0];
-        int presec=intervals[0][1];
+        int fir=intervals[0][0];
+        int sec=intervals[0][1];
         
         for(int i=1;i<intervals.size();i++)
         {
-            int fir=intervals[i][0];
-            int sec=intervals[i][1];
-            if(presec>=fir)
+            int f=intervals[i][0];
+            int s=intervals[i][1];
+            
+            if(f<=sec)
             {
-                presec=max(presec,sec);        
+                f=min(f,fir);
+                sec=max(sec,s);
             }
             else
             {
-                vector<int>v;
-                v.push_back(prefir);
-                v.push_back(presec);
-                res.push_back(v);
-                
-                // assigning them values
-                
-                presec=sec;
-                prefir=fir;
+                res.push_back({fir,sec});
+                fir=f;
+                sec=s;
             }
-         }
-                vector<int>v;
-                v.push_back(prefir);
-                v.push_back(presec);
-                res.push_back(v);
-                
+        }
+        
+        res.push_back({fir,sec});
         return res;
     }
 };
