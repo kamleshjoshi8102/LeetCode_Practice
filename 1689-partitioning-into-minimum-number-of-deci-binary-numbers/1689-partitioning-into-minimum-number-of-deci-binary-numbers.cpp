6ca9cb355ea64eccb8b9cc0110b77fb46c99e7fd
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-06-27 13:35:52 +0530

    Time: 52 ms (32.14%), Space: 13.7 MB (17.75%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -5,12 +5,10 @@ public:
     // just find maximum why? since we can add 1 upto that only\
     
     int minPartitions(string n) {
-        vector<int>v;
         int maxi=0;
         for(int i=0;i<n.size();i++)
         {
-            v.push_back(n[i]-'0');
-            maxi=max(maxi,v[i]);
+            maxi=max(maxi,n[i]-'0');
         }
         
         
