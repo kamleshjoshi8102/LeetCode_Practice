4c6f4ac74b0af2f72ffbf21e26a275448a5b1d17
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-06-28 11:21:41 +0530

    Time: 247 ms (5.05%), Space: 17.5 MB (21.80%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     int minDeletions(string s) {
         map<int,int>mp;
-        map<int,int>freq_cnt;
+        
         for(auto x:s)
         {
             mp[x]++;
         }
-        for(auto x:mp)
-        {
-            freq_cnt[x.first]=x.second;
-        }
+      
             
         set<int>se;
         int cn=0;
         
-        for(auto x:freq_cnt)
+        for(auto x:mp)
         {
             while(se.find(x.second)!=se.end() and x.second>0)
             {
