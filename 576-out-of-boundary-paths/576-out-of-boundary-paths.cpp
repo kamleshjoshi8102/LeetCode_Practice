45cd7e528b292f721a45c0f5f47e627cec77be04
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-07-16 10:58:24 +0530

    Time: 9 ms (79.68%), Space: 6.7 MB (69.13%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -4,31 +4,32 @@ public:
     long long ans=0;
     long long mod=1000000007;
     
-    int helper( int i , int j, int n , int m, int maxi , int cn)
+    int helper( int i , int j, int n , int m, int maxi)
     {
         if( i<0 or j<0 or i>=n or j>=m  )
         {
-            if(cn<=maxi)
+            if(maxi>=0)
             {
                 return 1;
             }
             return 0;
         }
-        if(cn>51)
+        if(maxi<0)
         {
             return 0;
         }
-        if(dp[i][j][cn]!=-1)
+        
+        if(dp[i][j][maxi]!=-1)
         {
-            return dp[i][j][cn];
+            return dp[i][j][maxi];
         }
         
-        int l = helper(i-1,j,n,m,maxi,cn+1);
-        int r = helper(i+1,j,n,m,maxi,cn+1);
-        int u = helper(i,j+1,n,m,maxi,cn+1);
-        int d = helper(i,j-1,n,m,maxi,cn+1);
+        int l = helper(i-1,j,n,m,maxi-1);
+        int r = helper(i+1,j,n,m,maxi-1);
+        int u = helper(i,j+1,n,m,maxi-1);
+        int d = helper(i,j-1,n,m,maxi-1);
         
-       return dp[i][j][cn]=(u%mod + l%mod + r%mod + d%mod)%mod;
+       return dp[i][j][maxi]=(u%mod + l%mod + r%mod + d%mod)%mod;
         
     }
     
@@ -37,7 +38,7 @@ public:
          
        memset(dp,-1,sizeof(dp));
         
-       return helper(startRow,startColumn,n,m,maxMove,0);
+       return helper(startRow,startColumn,n,m,maxMove);
         
         
     }
