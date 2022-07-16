d08eb70785974583a82afb7a0e54895cc6045140
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-07-16 10:56:18 +0530

    Time: 15 ms (57.00%), Space: 6.7 MB (71.79%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     int dp[55][55][55];
-    long long mod = 1e9 + 7;
-    int dfs(int i, int j, int n, int m, int x){
-        
-        if(i < 0 || i >= n || j < 0 || j >= m)
-            return 1;
-        if(x <= 0)
+    long long ans=0;
+    long long mod=1000000007;
+    
+    int helper( int i , int j, int n , int m, int maxi , int cn)
+    {
+        if( i<0 or j<0 or i>=n or j>=m  )
+        {
+            if(cn<=maxi)
+            {
+                return 1;
+            }
             return 0;
-        if(dp[i][j][x] != -1)
-            return dp[i][j][x];
+        }
+        if(cn>51)
+        {
+            return 0;
+        }
+        if(dp[i][j][cn]!=-1)
+        {
+            return dp[i][j][cn];
+        }
         
-        int up = dfs(i-1,j,n,m,x-1);
-        int down = dfs(i+1,j,n,m,x-1);
-        int left = dfs(i,j-1,n,m,x-1);
-        int right = dfs(i,j+1,n,m,x-1);
-        int total = (up % mod + down % mod + left % mod + right % mod) % mod;
-        dp[i][j][x] = total;
-        return dp[i][j][x];
+        int l = helper(i-1,j,n,m,maxi,cn+1);
+        int r = helper(i+1,j,n,m,maxi,cn+1);
+        int u = helper(i,j+1,n,m,maxi,cn+1);
+        int d = helper(i,j-1,n,m,maxi,cn+1);
+        
+       return dp[i][j][cn]=(u%mod + l%mod + r%mod + d%mod)%mod;
+        
     }
     
-    int findPaths(int n, int m, int maxMove, int Row, int Column) {
-        memset(dp, -1, sizeof(dp));
-        return dfs(Row,Column,n,m,maxMove);
+    
+    int findPaths(int n, int m, int maxMove, int startRow, int startColumn) {   
+         
+       memset(dp,-1,sizeof(dp));
+        
+       return helper(startRow,startColumn,n,m,maxMove,0);
+        
+        
     }
 };
