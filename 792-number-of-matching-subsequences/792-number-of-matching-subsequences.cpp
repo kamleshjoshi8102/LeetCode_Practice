b1134168e558683b3fa63d91ba6f937884073c42
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2022-07-20 16:13:34 +0530

    Time: 711 ms (22.05%), Space: 50 MB (45.03%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        
+        map<char,vector<int>>mp;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            mp[s[i]].push_back(i);        
+        }
+        
+        int res=0;
+        
+        for(int i=0;i<words.size();i++)
+        {
+             
+            string str=words[i];
+            int st=-1;
+            
+            for(int j=0;j<str.size();j++)
+            {
+                auto it=upper_bound(mp[str[j]].begin(),mp[str[j]].end(),st);
+                
+                
+                if(it==mp[str[j]].end())
+                {
+                    break;
+                }
+                
+                st=*it;
+                
+                if(j==str.size()-1)
+                {
+                    res++;
+                }
+                
+            }
+            
+        }
+    return res;
+        
+    }
+};
