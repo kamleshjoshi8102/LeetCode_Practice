63676a91276853b9fd6d27561a68555b0b1790b4
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2023-01-31 17:51:22 +0530

    Time: 176 ms (47.28%), Space: 29.9 MB (20.28%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -35,7 +35,7 @@ public:
                         while(j<k && nums[j]==nums[j+1]){j++;}
                         while(j<k && nums[k-1]==nums[k]){k--;}
                         j++;
-                        k--;
+                        // k--;
                     }
                     else if(su<0)
                     {
