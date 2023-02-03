c0ae118161477a719e535bf985961af4e6e5bdc4
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2023-02-03 11:32:38 +0530

    Time: 12 ms (67.82%), Space: 10.6 MB (53.67%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    string convert(string s, int nr) {
+    
+    if(nr <= 1) return s;
+
+    vector<string>v(nr, ""); 
+
+    int j = 0, dir = -1;
+
+    for(int i = 0; i < s.length(); i++)
+    {
+
+        if(j == nr - 1 || j == 0) dir *= (-1); 
+		 
+        v[j] += s[i];
+
+        if(dir == 1) j++;
+
+        else j--;
+    }
+
+    string answer;
+
+    for(auto &it : v) {
+        answer += it; 
+    }
+
+    return answer;
+
+    }
+};
