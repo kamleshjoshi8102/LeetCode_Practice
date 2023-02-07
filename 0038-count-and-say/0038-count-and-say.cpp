d491bebd23a573bb3f86f01367ead81da89a8d5c
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2023-02-07 08:34:33 +0530

    Time: 7 ms (77.29%), Space: 6.3 MB (99.17%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -6,15 +6,23 @@ public:
         
         for(int i=1; i<n; ++i)
         {
-            for(int j=0; j<res.size(); ++j){
+            for(int j=0; j<res.size(); ++j)
+            {
                 int cnt=1;
-                while(j+cnt<res.size() and res[j]==res[j+cnt]){
+                
+                
+                while(j+cnt<res.size() and res[j]==res[j+cnt])
+                {
                      ++cnt;
                 }
+                
                 j += (cnt-1);
+                
                 prev += to_string(cnt);
                 prev += res[j];
+            
             }
+            
             res = prev;
             
             prev = "";
