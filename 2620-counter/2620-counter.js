d33030c01cf160fc23937f43682d3e3a12bf5a57
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2023-05-06 12:51:55 +0530

    Time: 65 ms (13.87%), Space: 41.7 MB (78.55%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let count = n;
+    return function() {
+        
+        if(count==n)
+            {
+                count++;
+                return n;
+            }
+        else{
+            return count++;
+        }
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
