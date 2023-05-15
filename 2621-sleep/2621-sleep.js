a17c907ad1f96e136b6327e6801c1a2933256a2f
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2023-05-15 23:57:44 +0530

    Time: 64 ms (30.52%), Space: 41.2 MB (94.93%) - LeetHub

--- 2621-sleep/2621-sleep.js
+++ 2621-sleep/2621-sleep.js
@@ -7,5 +7,9 @@
  * sleep(100).then(() => console.log(Date.now() - t)) // 100
  */
 async function sleep(millis) {
-    await new Promise(resolve => setTimeout(resolve, millis));
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, millis)
+    });
 }
