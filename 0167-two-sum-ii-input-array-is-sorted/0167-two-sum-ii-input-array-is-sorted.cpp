3d31861a2b4349516ca004f0d6f08f8da3808c99
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2023-06-20 18:41:59 +0530

    Time: 32 ms (5.43%), Space: 15.6 MB (74.25%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    
+   int bn(vector<int> &arr , int l , int r , int x){
+        while(l <= r){
+            int m = l + (r - l) / 2;
+            if(arr[m] == x)
+                return m;
+            else if(arr[m] > x)
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        return -1;
+    }
+    vector<int> twoSum(vector<int>& numbers, int target) {
+         vector<int> ans(2);
+        int n = numbers.size();
+        for(int i = 0 ; i < numbers.size() - 1 ; i++){
+            int x = target - numbers[i];
+            int el = bn(numbers , i + 1 , n - 1 , x);
+            if(el != -1){
+                ans[0] = i + 1;
+                ans[1] = el + 1;
+            }
+        }
+        return ans;
+    }
+};
