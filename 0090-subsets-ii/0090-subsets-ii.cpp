40b30066010a7fc99b580953ba14c99cf361bb31
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2023-07-06 14:44:41 +0530

    Time: 12 ms (16.95%), Space: 17.2 MB (16.92%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    
+    void helper(int index,int n, vector<int>&nums, vector<int>temp, set<vector<int>>&ans)
+    {
+        if(index>=n)
+        {
+            // for(auto x:temp)
+            // {
+            //     cout<<x<<" ";
+            // }
+            sort(temp.begin(),temp.end());
+            ans.insert(temp);
+            return;
+        }
+        
+        // not present 
+        
+        helper(index+1,n,nums,temp,ans);
+        
+        // present
+        
+        temp.push_back(nums[index]);
+        
+        helper(index+1,n,nums,temp,ans);
+            
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        set<vector<int>>ans;
+        vector<vector<int>>res;
+        
+        helper(0,nums.size(),nums,{},ans);
+        
+        for(auto x:ans)
+        {
+            res.push_back(x);
+        }
+        
+        return res;
+        
+    }
+};
