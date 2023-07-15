b8f7af8797e736fc0b9fb8553b360208b2293584
Kamlesh Joshi <72374645+kamleshjoshi8102@users.noreply.github.com>
2023-07-15 11:04:20 +0530

    Time: 473 ms (65.63%), Space: 156.2 MB (88.24%) - LeetHub

--- 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.cpp
@@ -1,82 +1,45 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     
-    
-    ListNode* reverse(ListNode*head)
-    {
+    // Function to reverse a linked list
+    ListNode* reverse(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* prev = nullptr;
         
-        ListNode*temp = head , *prev=head;
-        
-        if(head->next==nullptr)
-        {
-            return head;
+        while (head) {
+            ListNode* curr = head->next;
+            head->next = prev;
+            prev = head;
+            head = curr;
         }
         
-        while(head)
-        {
-            if(temp==head)
-            {
-                ListNode*curr = head->next;
-                head->next=nullptr;
-                prev=head;
-                head=curr;
-            }
-            else if(head->next==nullptr)
-            {
-                head->next=prev;
-                return head;
-            }
-            else
-            {
-                ListNode*curr = head->next;
-                head->next=prev;
-                prev=head;
-                head=curr;
-            }
-        }
-        return head;
+        return prev;
     }
     
-    
     ListNode* removeNodes(ListNode* head) {
+        // Reverse the linked list
         head = reverse(head);
         
-        ListNode*temp = head, *prev=head;
+        ListNode* temp = head;
+        ListNode* prev = head;
         
-        int maxi = INT_MIN;
+        int maxi = INT_MIN; // Initialize the maximum value
         
-        while(temp)
-        {
-            if(temp->val<maxi)
-            {
-                prev->next=temp->next;
-                maxi=max(maxi,temp->val);
-                temp=temp->next;
-                
+        while (temp) {
+            if (temp->val < maxi) {
+                // Remove the node
+                prev->next = temp->next;
+                temp = temp->next;
+            } else {
+                // Update the maximum value and move to the next node
+                maxi = max(maxi, temp->val);
+                prev = temp;
+                temp = temp->next;
             }
-            else
-            {
-                
-                maxi=max(maxi,temp->val);
-                prev=temp;
-                temp=temp->next;
-            }
-            
         }
-         
         
-        head=reverse(head);
-        
+        // Reverse the linked list again to restore the original order
+        head = reverse(head);
         
         return head;
     }
